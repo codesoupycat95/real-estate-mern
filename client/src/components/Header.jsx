7beb37d8b69c425045f4d6bae25c1214b88526c3
Devam <kratoskakdiya@gmail.com>
2024-03-17 18:27:32 +0530

    added navbar/header and styling it

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import {FaSearch} from 'react-icons/fa';
+
+export const Header = () => {
+    return (
+        <>
+            <header className="bg-slate-200 shadow-md">
+                <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                        <span className="text-slate-500">Kakdiya</span>
+                        <span className="text-slate-700">Estate</span>
+                    </h1>
+                    <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                        <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+                        <FaSearch className='text-slate-600'/>
+                    </form>
+                    <ul></ul>
+                </div>
+            </header>
+        </>
+    );
+}
