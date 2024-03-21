b10d1bd08821235cd55f1ebd01ac0b60071f027a
Devam <kratoskakdiya@gmail.com>
2024-03-21 23:41:16 +0530

    added a file

--- api/index.js
+++ api/index.js
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_CONN).then(()=>{
+mongoose.connect(process.env.MONGO_CONN).then(() => {
     console.log("Connection to database is successful");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
-const app=express();
+const app = express();
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000!!!");
 });
 
