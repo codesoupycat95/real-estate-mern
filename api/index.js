c609a4359f55efb8182258983255507defa770c0
Devam <kratoskakdiya@gmail.com>
2024-03-21 23:26:03 +0530

    connected the project to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONN).then(()=>{
+    console.log("Connection to database is successful");
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app=express();
 
