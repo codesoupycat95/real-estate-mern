3aa837685a3668f3537ec3799ea63f69e30923d3
Devam <kratoskakdiya@gmail.com>
2024-03-22 23:26:10 +0530

    created a test api for route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";  // Now because you are adding '.' in your filename way before extension and just before declaring extension after name, so there are two dots in file name which will create confusion for server if you don't mention `.js` in your imported file name. Otherwise normally we don't mention .`js`
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_CONN).then(() => {
@@ -15,3 +16,4 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000!!!");
 });
 
+app.use("/api/user", userRouter);
