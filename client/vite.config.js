20473ab0db8e85677837df07e35b763506ee2aab
Devam <kratoskakdiya@gmail.com>
2024-03-27 23:27:59 +0530

    Completed Signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {                      // You can even use single-quotes in place of double quotes
+                target: "http://localhost:3000",
+                secure: false,
+            },
+        },
+    },
+    plugins: [react()],
 })
