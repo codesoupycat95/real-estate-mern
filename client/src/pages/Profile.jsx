903fb49689fd2ff712deae5df811096fc90ebcf6
Devam <kratoskakdiya@gmail.com>
2024-06-02 21:33:35 +0530

    Complete Image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,78 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { app } from "../firebase.js";
 
 export const Profile = () => {
     const { currentUser } = useSelector((state) => state.user);
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+    const [filePercentage, setFilePercentage] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+
+    // Firebase Image Storage Rules::
+    // allow read;
+    // allow write: if
+    // request.resource.size < 2 * 1024 * 1024 &&
+    // request.resource.contentType.matches('images/.*')
+
+    // Firebase issue still persists and for now the rule has changed to only read and write and it works for now
+    // It will scan anything and will store anything as there's the issue with the firebase rules.
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state changed', (snapshot) => {                // snapshot here is a piece of information which we get on each state change
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setFilePercentage(Math.round(progress));
+        },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) =>
+                        setFormData({ ...formData, avatar: downloadURL })
+                    );
+            });
+    }
 
     return (
         <>
             <div className="p-3 max-w-lg mx-auto">
                 <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
                 <form className="flex flex-col gap-4">
-                    <img src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+                    <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
+                    <img
+                        onClick={() => fileRef.current.click()}
+                        src={formData.avatar || currentUser.avatar} alt="profile"
+                        className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+                    />
+                    <p className="text-sm self-center">    {/* "text-center" class also works here */}
+                        {fileUploadError ? (
+                            <span className="text-red-700">Error Image Upload
+                                (Image must be less than 2 megabytes)</span>
+                        ) : filePercentage > 0 && filePercentage < 100 ? (
+                            <span className="text-slate-700">
+                                {`Uploading ${filePercentage}`}
+                            </span>
+                        ) : filePercentage === 100 ? (
+                            <span className="text-green-700">Image Successfully Uploaded!</span>
+                        ) : (
+                            ""
+                        )
+                        }
+                    </p>
                     <input type="text" placeholder="username" id="username" className="boder p-2 rounded-lg " />
                     <input type="email" placeholder="email" id="email" className="boder p-2 rounded-lg " />
                     <input type="password" placeholder="password" id="password" className="boder p-2 rounded-lg " />
@@ -18,7 +82,7 @@ export const Profile = () => {
                     <span className="text-red-700 capitalize cursor-pointer">delete account</span>
                     <span className="text-red-700 capitalize cursor-pointer">sign out</span>
                 </div>
-            </div>
+            </div >
         </>
     );
 }
