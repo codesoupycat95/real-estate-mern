989353f1f5020827ae24b43f968ff1959e5bdb34
Devam <kratoskakdiya@gmail.com>
2024-06-14 20:23:36 +0530

    Completed update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { app } from "../firebase.js";
+import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
 
 export const Profile = () => {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser, loading, error } = useSelector((state) => state.user);
     const fileRef = useRef(null);
     const [file, setFile] = useState(undefined);
     const [filePercentage, setFilePercentage] = useState(0);
     const [fileUploadError, setFileUploadError] = useState(false);
     const [formData, setFormData] = useState({});
+    const [updateSuccess, setUpdateSuccess] = useState(false);
+    const dispatch = useDispatch();
 
     // Firebase Image Storage Rules::
     // allow read;
@@ -47,11 +50,41 @@ export const Profile = () => {
             });
     }
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();     // preventDefault() prevents the default behaviour which is refresh of the page here during submission.
+        try {
+            dispatch(updateUserStart());
+            const res = await fetch(`/api/user/update/${currentUser._id}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+
+            if (data.success === false) {
+                dispatch(updateUserFailure(data.message));
+                return;
+            }
+
+            dispatch(updateUserSuccess(data));
+            setUpdateSuccess(true);
+        }
+        catch (error) {
+            dispatch(updateUserFailure(error.message));
+        }
+    }
+
     return (
         <>
             <div className="p-3 max-w-lg mx-auto">
                 <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-                <form className="flex flex-col gap-4">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                     <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
                     <img
                         onClick={() => fileRef.current.click()}
@@ -73,15 +106,20 @@ export const Profile = () => {
                         )
                         }
                     </p>
-                    <input type="text" placeholder="username" id="username" className="boder p-2 rounded-lg " />
-                    <input type="email" placeholder="email" id="email" className="boder p-2 rounded-lg " />
-                    <input type="password" placeholder="password" id="password" className="boder p-2 rounded-lg " />
-                    <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 ">update</button>
+                    <input type="text" placeholder="username" id="username" defaultValue={currentUser.username} onChange={handleChange} className="boder p-2 rounded-lg " />
+                    <input type="email" placeholder="email" id="email" defaultValue={currentUser.email} onChange={handleChange} className="boder p-2 rounded-lg " />
+                    <input type="password" placeholder="password" id="password" defaultValue={currentUser.password} onChange={handleChange} className="boder p-2 rounded-lg " />
+                    <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 ">
+                        {loading ? "Loading..." : "Update"}
+                    </button>
                 </form>
                 <div className="flex justify-between mt-5">
                     <span className="text-red-700 capitalize cursor-pointer">delete account</span>
                     <span className="text-red-700 capitalize cursor-pointer">sign out</span>
                 </div>
+
+                <p className="text-red-700 mt-5">{error ? error : ""}</p>
+                <p className="text-green-700 mt-5">{updateSuccess?"User is updated successfully!":""}</p>
             </div >
         </>
     );
